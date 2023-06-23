e554f412800003c7a4595db39b1969abf90b7596
Ildar Sahautdinov <scams1992@gmail.com>
2023-06-23 16:27:13 +0500

    release v1

--- index.js
+++ index.js
@@ -3,91 +3,129 @@ import { config } from 'dotenv'
 import express from 'express'
 import { GoogleSpreadsheet } from 'google-spreadsheet'
 import TelegramBot from 'node-telegram-bot-api';
-// import request from './src/requestApi.js';
-
-
-// const JOKE_API = 'https://v2.jokeapi.dev/joke/Programming?type=single';
-// const TELEGRAM_URI = `https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}`;
+import request from './src/requestApi.js';
+import { Bot } from 'grammy';
 
 const token = '5570994601:AAFGHJo3crLnHA3XBPtF_5sclnD1EoPlh0Y'; // тут токен кторый мы получили от botFather
 
 // включаем самого обота
 const bot = new TelegramBot(token, {polling: true});
+const bot1 = new Bot(token);
 
-//конфиг клавиатуры
-const keyboard = [
-    [
-      {
-        text: 'Хочу кота', // текст на кнопке
-        callback_data: 'cats' // данные для обработчика событий
-      }
-    ],
-    [
-        {
-          text: 'Хочу песика',
-          callback_data: 'dogs'
-        }
-    ]
-    // [
-    //     {
-    //       text: 'Хочу проходить курсы',
-    //       url: 'lol' //внешняя ссылка
-    //     }
-    //   ]
-  ];
+bot1.api.setMyCommands([
+  { command: "gif", description: "start" },
+  // { command: "help", description: "h"},
+  // { command: "list", description: "l" },
+  ]);
 
-// обработчик события присылания нам любого сообщения
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
+// конфиг клавиатуры
+// const keyboard = [
+//     [{
+//         text: 'Хочу кота', // текст на кнопке
+//         callback_data: 'cats' // данные для обработчика событий
+//       }]
+// ];
 
-  // отправляем сообщение
-  bot.sendMessage(chatId, 'Привет, Друг! чего хочешь?', { // прикрутим клаву
-        reply_markup: {
-            inline_keyboard: keyboard
-        }
-    });
+bot.onText(/\/gif/, async (msg) => {
+  const chatId = msg.chat.id;
+  // console.log(msg)
+  let img = '';
+
+  const input = msg.text.split(' ');
+
+  const randomNum = Math.floor(Math.random() * 30);
+  const gifApi = 'HvnTVXXC8uv8fy4iA9WVjl2tUd1MAldb';
+  const gifUrl = `https://api.giphy.com/v1/gifs/search?q=${input[1]}&rating=g&api_key=${gifApi}`;
+  
+  const result = await fetch(gifUrl)
+  .then(function(data) {
+    return data.json()
+  })
+  .then(function(json){
+  //   console.log(json.data[0].images.fixed_height.url)
+    const imgPath = json.data[randomNum].images.fixed_height.url
+    return imgPath;
+  })  
+
+  bot.sendSticker(chatId, result);
+
+  // if (query.data === 'cats') { // если кот
+  //     img = result;
+  // }
+
+  // if (query.data === 'dogs') { // если пёс
+  //     img = result;
+  // }
+  
+  // if (img) {
+  //     bot.sendSticker(chatId, img, { // прикрутим клаву
+  //         // reply_markup: {
+  //         //     inline_keyboard: keyboard
+  //         // }
+  //     });
+  // } else {
+  //     bot.sendMessage(chatId, 'Непонятно, давай попробуем ещё раз?', { // прикрутим клаву
+  //         reply_markup: {
+  //             inline_keyboard: keyboard
+  //         }
+  //     });
+  // }
 });
+
+
+// // обработчик событий нажатий на клавиатуру
+// bot.on('callback_query', async (query) => {
+//   console.log('____',query)
+//     const chatId = query.message.chat.id;
+
+//     let img = '';
+
+//     const input = query.data;
+//     const randomNum = Math.floor(Math.random() * 30);
+//     const gifApi = 'HvnTVXXC8uv8fy4iA9WVjl2tUd1MAldb';
+//     const gifUrl = `https://api.giphy.com/v1/gifs/search?q=${input}&rating=g&api_key=${gifApi}`;
     
+//     const result = await fetch(gifUrl)
+//     .then(function(data) {
+//       return data.json()
+//     })
+//     .then(function(json){
+//     //   console.log(json.data[0].images.fixed_height.url)
+//       const imgPath = json.data[randomNum].images.fixed_height.url
+//       return imgPath;
+//     })  
 
-// обработчик событий нажатий на клавиатуру
-bot.on('callback_query', async (query) => {
-    const chatId = query.message.chat.id;
+//     if (query.data === 'cats') { // если кот
+//         img = result;
+//     }
 
-    let img = '';
-
-    const input = query.data;
-    const gifApi = 'HvnTVXXC8uv8fy4iA9WVjl2tUd1MAldb';
-    const gifUrl = `https://api.giphy.com/v1/gifs/search?q=${input}&rating=g&api_key=${gifApi}`;
+//     if (query.data === 'dogs') { // если пёс
+//         img = result;
+//     }
     
-    const result = await fetch(gifUrl)
-    .then(function(data) {
-      return data.json()
-    })
-    .then(function(json){
-    //   console.log(json.data[0].images.fixed_height.url)
-      const imgPath = json.data[0].images.fixed_height.url
-      return imgPath;
-    })  
+//     if (img) {
+//         bot.sendSticker(chatId, img, { // прикрутим клаву
+//             // reply_markup: {
+//             //     inline_keyboard: keyboard
+//             // }
+//         });
+//     } else {
+//         bot.sendMessage(chatId, 'Непонятно, давай попробуем ещё раз?', { // прикрутим клаву
+//             reply_markup: {
+//                 inline_keyboard: keyboard
+//             }
+//         });
+//     }
+//   });
 
-    if (query.data === 'cats') { // если кот
-        img = result;
-    }
 
-    if (query.data === 'dogs') { // если пёс
-        img = result;
-    }
-    
-    if (img) {
-        bot.sendSticker(chatId, img, { // прикрутим клаву
-            // reply_markup: {
-            //     inline_keyboard: keyboard
-            // }
-        });
-    } else {
-        bot.sendMessage(chatId, 'Непонятно, давай попробуем ещё раз?', { // прикрутим клаву
-            reply_markup: {
-                inline_keyboard: keyboard
-            }
-        });
-    }
-  });
+  // bot.on('message', (msg) => {
+  //   const chatId = msg.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
+  
+  //   // отправляем сообщение
+  //   // bot.sendMessage(chatId, 'Привет, Друг! чего хочешь?', { // прикрутим клаву
+  //   //       reply_markup: {
+  //   //           inline_keyboard: keyboard
+  //   //       }
+  //   //   });
+  // }); 
